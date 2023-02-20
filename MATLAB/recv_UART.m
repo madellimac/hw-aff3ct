4e87afe73f9d886b90a40d840ec23dcbb1f2f05f
LEROUX Camille <cleroux@juliett.pedago.ipb.fr>
2023-02-20 15:52:03 +0100

    UART loop test ok.

---
+++ MATLAB/recv_UART.m
@@ -0,0 +1,13 @@
+function vec = recv_UART(s, size)
+
+  vec = zeros(1,size);
+  
+  vec = fread(s,size);
+  %for i = 1:size
+  %     vec(i) = fread(s,1);
+  %     pause(0.01) % wait 1ms to let the data arrive to the FPGA
+  %end
+
+fclose(s);
+delete(s)
+clear s
