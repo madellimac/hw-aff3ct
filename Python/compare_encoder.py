121bf2ccd769456d78b501e5b4e85f5e4aa8dbae
AP67540 <william.farah-lajoie.1@ens.etsmtl.ca>
2023-05-11 17:10:04 +0200

    compare encoder

---
+++ Python/compare_encoder.py
@@ -0,0 +1,47 @@
+from typing import List
+import sys
+sys.path.insert(1, 'encoder')
+sys.path.insert(1, 'UART')
+
+# Import the python encoder
+import encoder as my_encoder
+# Import the vhdl encoder
+import send_recv_FPGA as vhdl_encoder
+
+# function that takes an array of char in parameter 
+
+def compare_char_encoder(data_in: List[chr]):
+    """Function that get the encoded char values from the python encoder and vhdl encoder and compare them
+    for a given list of char
+
+    Args:
+        data_in (List[chr]): list of char to encode
+    """
+    # vhdl encoder
+    encoded_bits_vhdl = vhdl_encoder.send_rec_FPGA(data_in)
+
+    N=16 # for a char, the encoded output is 16 bits
+    encoded_bits = []
+
+    for chr in data_in:
+        # My encoder
+        encoded_bits.append(my_encoder.encode_char(chr))
+        
+    # Compare lenght
+    if(len(encoded_bits_vhdl) != len(encoded_bits)):
+        print("error: length mismatch")
+        print("vhdl length: " + len(encoded_bits_vhdl) + " | my encoder length: " + len(encoded_bits))
+        return
+
+    # compare the output of the two encoder
+    for i in range(len(encoded_bits_vhdl)):
+        if(encoded_bits_vhdl[i] != encoded_bits[i]):
+            print("error")
+            print("vhdl index: " + i + " | bit value: " + encoded_bits_vhdl[i])
+            print("My encoder index : " + i + " | bit value: " + encoded_bits[i])
+            break
+
+
+if __name__ == "__main__":
+    img_array = vhdl_encoder.read_image_grayscale("images/baboon.jpg")
+    compare_char_encoder(img_array)
